d2f83e5c9a00e896263edca72546077756a63e95
Romi Fadhurrohman Nabil <70937653+Ragnarom-kun@users.noreply.github.com>
2024-11-19 16:36:23 +0700

    feat: init app clip

---
+++ AudiumClip/AudiumClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiumClipApp.swift
+//  AudiumClip
+//
+//  Created by Romi Fadhurohman Nabil on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AudiumClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAppClip/TestAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppClipApp.swift
+//  TestAppClip
+//
+//  Created by Romi Fadhurohman Nabil on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
